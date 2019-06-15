0f18134b7d30ae596de91520a91fdeeb18dc6b26
David Dashifen Kees <dashifen@dashifen.com>
2019-06-15 11:27:27 -0400

    theme index.php exception handling refactor

--- docroot/wp-content/themes/shadowlab/index.php
+++ docroot/wp-content/themes/shadowlab/index.php
@@ -4,10 +4,9 @@
 require ABSPATH . "../vendor/autoload.php";
 
 use Aura\Di\ContainerBuilder;
+use Dashifen\Response\ResponseException;
 use Dashifen\Response\View\ViewException;
 use Dashifen\Router\RouterException;
-use Dashifen\Response\ResponseException;
-use Aura\Di\Exception\SetterMethodNotFound;
 
 /**
  * @var \Dashifen\Exceptionator\ExceptionatorInterface  $exceptionator
@@ -39,67 +38,46 @@ try {
   $route = $shadowlab->newInstance('Dashifen\Router\Router')->route();
   $action = $shadowlab->newInstance($route->getAction());
   $response = $action->execute($route->getActionParameter());
-} catch (RouterException $routerException) {
+} catch (Exception $exception) {
 
-  // there's a type of router exception that we want to handle here: an
-  // unexpected route.  if we have that, we'll force our 404 response here.
-  // we need this because we've completely abandoned the WP router for this
-  // theme so we can't rely on it to take us to a 404.php template.  any
-  // other type of RouterException we'll pass on as general error response.
-  // the false flag overrides the default behavior (dying) and returns the
-  // message instead.
+  // there's one type of Exception we want to handle with care: a 404 page.
+  // everything else just gets dumped to the screen.  notice that we have a
+  // nested try/catch block here; that's a bad code smell.  but, it's
+  // because, when an exception is thrown, we still need to construct our
+  // response.  that may, in turn, throw new exceptions.  but, we can
+  // safely ignore them (see below).
 
   try {
     $response = $shadowlab->newInstance('Shadowlab\Framework\Response\Response');
 
-    if ($routerException->getCode() === RouterException::UNEXPECTED_ROUTE) {
+    if ($exception instanceof RouterException && $exception->getCode() === RouterException::UNEXPECTED_ROUTE) {
 
       // the format of this exception's message is Route: <method>;<path>.  we
       // only need the method and path, so we'll extract that as we prepare our
       // response.
 
       $response->handleNotFound();
-      $message = $routerException->getMessage();
+      $message = $exception->getMessage();
       $message = substr($message, strpos($message, ":") + 1);
       $response->setDatum("route", $message);
       $response->setTemplate();
     } else {
 
-      // otherwise, this is a general error, not a 404, and we'll tell it
-      // to handle the error case.  but, we'll want to send it the message
-      // to print based on this exception.  the false flag we send to the
-      // exceptionator will force it to return the message it builds instead
-      // of dying.
+      // otherwise, we'll let our exceptionator give us a pretty print out
+      // of our exceptional situation and use that as our error message.
 
-      $message = $exceptionator->exceptionHandler($routerException, false);
+      $message = $exceptionator->exceptionHandler($exception, false);
       $response->handleError($message);
       $response->setTemplate();
     }
   } catch (ResponseException | ViewException $exception) {
 
-    // nested try/catch blocks is a pretty bad code smell, bit in this case
-    // both the response and view exception that could be thrown is about
-    // altering either of them after the view is compiled.  since we know
-    // that hasn't happened yet, we can safely ignore these exceptions at
-    // this time.
+    // these should never happen; they would indicate a change to either
+    // our response or view after it's been compiled.  but, since that
+    // hasn't happened yet, we shouldn't ever find ourselves here.  but,
+    // if we ever do, we'll just let our exceptionator handle it.
 
-  }
-} catch (SetterMethodNotFound $setterNotFoundException) {
-
-  // like above, here we can't do much other than  print our message.  we'll
-  // get it with our exceptionator and then pass it to our response.
-
-  try {
-    $message = $exceptionator->exceptionHandler($setterNotFoundException, false);
-    $response = $shadowlab->newInstance('Shadowlab\Framework\Response\Response');
-    $response->handleError($message);
-    $response->setTemplate();
-  } catch (ResponseException | ViewException $viewException) {
-
-    // also like above, we don't have to worry about these.  we still haven't
-    // compiled our responses, so it's unnecessary for us do anything here
-    // since they won't be thrown at this time.
-
+    $exceptionator->exceptionHandler($exception);
   }
 }
 
