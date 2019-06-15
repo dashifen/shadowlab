309c0b11fc0b1b4c010391e9afb5322741d86d82
David Dashifen Kees <dashifen@dashifen.com>
2019-06-15 11:13:59 -0400

    version bumped

--- docroot/wp-content/plugins/shadowlab-cheatsheets/index.php
+++ docroot/wp-content/plugins/shadowlab-cheatsheets/index.php
@@ -4,5 +4,5 @@ Plugin Name: Shadowlab Cheatsheets
 Description: The plugin that controls the GM/Player cheatsheets for Shadowlab
 Author: David Dashifen Kees
 Author URI: https://dashifen.com
-Version: 0.0.2
+Version: 0.0.3
 */
--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ gulp.task('bump', function (complete) {
   gulp.src('./docroot/wp-content/themes/shadowlab/style.css').pipe(bump())
     .pipe(gulp.dest('./docroot/wp-content/themes/shadowlab/'));
 
-  gulp.src('./docroot/wp-content/plugins/cheatsheet/style.css').pipe(bump())
-    .pipe(gulp.dest('./docroot/wp-content/plugins/cheatsheet/'));
+  gulp.src('./docroot/wp-content/plugins/shadowlab-cheatsheets/index.php').pipe(bump())
+    .pipe(gulp.dest('./docroot/wp-content/plugins/shadowlab-cheatsheets/'));
 
   gulp.src('./composer.json').pipe(bump()).pipe(gulp.dest('./'));
   gulp.src('./package.json').pipe(bump()).pipe(gulp.dest('./'));
