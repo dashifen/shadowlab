70463760d326994c5eb7c99912c1ebe1f13a3a92
David Dashifen Kees <dk1041@georgetown.edu>
2019-08-09 15:48:29 -0400

    nascent router added to theme index

--- docroot/wp-content/themes/shadowlab/index.php
+++ docroot/wp-content/themes/shadowlab/index.php
@@ -1,3 +1,14 @@
 <?php
 
-\Shadowlab\Theme\Theme::debug($GLOBALS["wp_query"]);
+use Shadowlab\Framework\Router;
+use Dashifen\Exception\Exception;
+use Shadowlab\Framework\Controller;
+
+try {
+  $controller = new Controller();
+  $router = new Router($controller);
+  $template = $router->getTemplate();
+  $template->show();
+} catch (Exception $e) {
+  $controller->catcher($e);
+}
